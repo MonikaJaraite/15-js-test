f44f59259414626f16b7b4b0ddcf9639ee84a08c
MonikaJaraite <monjara@gmail.com>
2023-03-03 12:31:33 +0200

    ex. 5

--- js/ex-05.js
+++ js/ex-05.js
@@ -2,3 +2,15 @@ console.clear();
 
 // Naudojant while ciklą, spausdinti atsitiktinius skaičius nuo 1 iki 10. Paskutinis atspausdintas skaičius turi būti 5. (7 taškai)
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+let i = 0;
+
+while (i != 5) {
+    i = getRandomInt(1, 10);
+    console.log(i);
+}
