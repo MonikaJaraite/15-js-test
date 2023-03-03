0592008c5b31c2de475c39d299bd166caa3ee4fe
MonikaJaraite <monjara@gmail.com>
2023-03-03 12:48:00 +0200

    ex. 06

--- js/ex-06.js
+++ js/ex-06.js
@@ -2,3 +2,16 @@ console.clear();
 
 // Sukurti masyvą, kurio ilgis būtų nuo 20 iki 30, o reikšmės būtų skaičiai nuo 10 iki 30. Surasti didžiausią masyvo reikšmę, NENAUDOJANT sort() bei Math.max() funkcijų. (7 taškai)
 
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const arr = [];
+
+for (let i = 0; i <= rand(20, 30); i++) {
+    arr.push(rand(10, 30));
+}
+
+console.log(arr);
