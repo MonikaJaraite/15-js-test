5a89aeeb0ef21bf5aa925c4059a6e0fb55d6dcc8
MonikaJaraite <monjara@gmail.com>
2023-03-03 12:13:21 +0200

    ex. 4

--- js/ex-04.js
+++ js/ex-04.js
@@ -2,3 +2,12 @@ console.clear();
 
 // Naudojant for ciklą, sugeneruoti penkis atsitiktinius skaičius nuo 1 iki 10. Išvesti juos konsolėje. (5 taškai)
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+for (let i = 1; i <= 5; i++) {
+    console.log(getRandomInt(1,10));
+}
