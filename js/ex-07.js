d94312e0492f170e9c67640e1e8b672785e3ea5e
MonikaJaraite <monjara@gmail.com>
2023-03-03 13:08:59 +0200

    ex. 7

--- js/ex-07.js
+++ js/ex-07.js
@@ -2,3 +2,21 @@ console.clear();
 
 // Sugeneruokite masyvą, kurio reikšmės atsitiktinės raidės A, B, C ir D, o ilgis 100. Suskaičiuokite kiek yra kiekvienos raidės. (7 taškai)
 
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function generateRandomLetter() {
+    const alphabet = "ABCD"
+    return alphabet[Math.floor(Math.random() * alphabet.length)]
+}
+
+const arr = [];
+
+for (let i = 0; i <= 100; i++) {
+    arr.push(generateRandomLetter(1, 4));
+}
+
+console.log(arr);
